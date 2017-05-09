5e7c8786d11d27e962958ce1f222570a3ea0fb22
lukasz.szymszon <lukasz.szymszon@gmail.com>
2017-05-09 20:44:34 +0200

    Added Mute button.

---
+++ Assets/Scripts/MuterAudio.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Assets.Scripts
+{
+    public class MuterAudio : MonoBehaviour
+    {
+        public AudioListener AudioListener;
+        public Image[] ImageVoulme;   
+        public void InvertVolume()
+        {
+            foreach (var image in ImageVoulme)
+            {
+                image.enabled = !image.enabled;
+            }
+            AudioListener.enabled = !AudioListener.enabled;
+        }
+    }
+}
